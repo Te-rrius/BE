9a13862e266b4746cf0552daf8a37ec826f2075f
Seong Jin <oroi2009@gmail.com>
2026-05-20 18:50:09 +0900

    fix : 내 리포트 전체 조회 -> 매치비디오 ID 값 반환 추가

--- src/main/java/hansung/org/terrius/domain/report/web/dto/MyReportRes.java
+++ src/main/java/hansung/org/terrius/domain/report/web/dto/MyReportRes.java
@@ -8,11 +8,13 @@ import java.time.LocalDate;
 @Builder
 public record MyReportRes(
         Long reportId,
+        Long matchVideoId,
         LocalDate matchDate
 ) {
     public static MyReportRes from(ReportOwnership reportOwnership) {
         return MyReportRes.builder()
                 .reportId(reportOwnership.getReport().getId())
+                .matchVideoId(reportOwnership.getReport().getMatchVideo().getId())
                 .matchDate(reportOwnership.getReport().getMatchVideo().getMatchDate())
                 .build();
     }
