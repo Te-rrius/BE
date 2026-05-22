0d91f29666d5dcf3d7d12478daf1554183856862
ThreeeJ <2171266@hansung.ac.kr>
2026-05-22 14:39:31 +0900

    feat: 구장 상세 조회 API 구현

--- src/main/java/hansung/org/terrius/domain/stadium/repository/CourtRepository.java
+++ src/main/java/hansung/org/terrius/domain/stadium/repository/CourtRepository.java
@@ -4,6 +4,7 @@ import hansung.org.terrius.domain.stadium.entity.Court;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -12,4 +13,6 @@ public interface CourtRepository extends JpaRepository<Court, Long> {
     Optional<Court> findFirstByStadiumIdOrderByCourtNumberAsc(Long stadiumId);
 
     boolean existsByStadiumIdAndCourtNumber(Long stadiumId, Integer courtNumber);
+
+    List<Court> findAllByStadiumIdOrderByCourtNumberAsc(Long stadiumId);
 }
